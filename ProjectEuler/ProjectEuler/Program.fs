3b213d2a28e2006e9d4cf4c6ab5fc5aea1bc5e49
colmprunty <colmpruntysignups+github@gmail.com>
2015-09-01 20:08:46 +0100

    Problem 1

---
+++ ProjectEuler/ProjectEuler/Program.fs
@@ -0,0 +1,9 @@
+﻿let mutable sum = 0
+
+let sumFunctions() =
+    for i in 1 .. 999 do
+        if i%3 = 0 || i%5 = 0 then
+            sum <- sum + i
+
+sumFunctions()
+System.Console.WriteLine(sum)
