69f8411dc8beaa115f25d0665adea3cbeea87fda
colmprunty <colmpruntysignups+github@gmail.com>
2016-01-01 20:08:09 +0000

    some problem 2 stuff

---
+++ ProjectEuler/ProjectEuler/Problem2.fs
@@ -0,0 +1,17 @@
+﻿namespace Problem2
+
+module Problem2 =
+    let mutable x = 1
+    let mutable y = 2    
+
+    let getNextFibbonaci() =
+        let n = x + y
+        x <- y
+        y <- n
+        System.Console.WriteLine(y)
+
+    let doTheThing() = 
+        while x < 4000000
+            do getNextFibbonaci()
+
+    doTheThing()
